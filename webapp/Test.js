f67ade566da6e6281468162736c1acea76565504
tfey <thorstenfey@outlook.com>
2020-10-13 12:59:54 +0000

    Lazy Upload

---
+++ webapp/Test.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/base/Object",
+	"sap/m/MessageToast"
+], function (Object, MessageToast) {
+	"use strict";
+
+	return Object.extend("HelloWorld.Sample", {
+
+		doSomething: function () {
+			var sHelloWorld = "Hello world";
+
+			MessageToast.show(sHelloWorld, {duration: 1000});
+
+			this.oObject = 5;
+			return;
+		}
+	});
+});
