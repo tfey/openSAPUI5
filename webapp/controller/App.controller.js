7c13b13763988cd5d31ee55370f9a3cdcd5b7468
tfey <thorstenfey@outlook.com>
2020-10-13 18:10:38 +0000

    Initial Upload

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,17 +1,37 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Log, JSONModel, formatter, filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
+		
+		formatter: formatter,
+		
 		onInit: function () {
-
+			Log.info("Controller has been initialized!");
+		},
+		
+		onBefore: function () {
+			Log.info("The view will be shortly rendered!");
+		},
+		
+		onAfterRendering: function () {
+			Log.info("The view has been rendered!");
 		},
 		
 		onPress: function (sValue) {
 			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
 				oMessage.show("Searching..." + " " + sValue);
 			});
+		},
+		
+		onExit: function () {
+			Log.info("Controller will shortly be destroyed");
 		}
 	});
 });
